ceb09b505263d999e9dd92e3d9dad3493d244861
kauan-cancelier <kauanmellocancelier2005@gmail.com>
2024-10-20 21:36:48 -0300

    Adicionado métodos para aumentar cobertura de testes de partes envolvidas.

--- src/test/java/com/attus/processmanager/controllers/StakeholderControllerTest.java
+++ src/test/java/com/attus/processmanager/controllers/StakeholderControllerTest.java
@@ -1,13 +1,17 @@
 package com.attus.processmanager.controllers;
 
 import com.attus.processmanager.controller.StakeholderController;
+import com.attus.processmanager.dto.StakeholderSaveRequest;
+import com.attus.processmanager.dto.StakeholderUpdateRequest;
 import com.attus.processmanager.models.Stakeholder;
 import com.attus.processmanager.models.enums.StakeholderType;
 import com.attus.processmanager.service.StakeholderService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -17,6 +21,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.Collections;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Slf4j
@@ -62,6 +67,31 @@ class StakeholderControllerTest {
     }
 
     @Test
+    void testBadRequestWhenCreate() throws Exception {
+        Mockito.when(service.save(null)).thenThrow(new NullPointerException());
+
+        mockMvc.perform(post(URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(null)))
+                        .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testReturnBadRequestWhenCpfAndCnpjAreBothProvided() throws Exception {
+        StakeholderSaveRequest stakeholderRequest = new StakeholderSaveRequest();
+        stakeholderRequest.setCpf("12345678901");
+        stakeholderRequest.setCnpj("12345678000199");
+
+        Mockito.when(service.save(Mockito.any(Stakeholder.class)))
+                .thenThrow(new IllegalArgumentException());
+
+        mockMvc.perform(post("/stakeholders")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(stakeholderRequest)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
     void testUpdate() throws Exception {
         Stakeholder updatedStakeholder = createStakeholder();
         updatedStakeholder.setName("Updated Stakeholder");
@@ -76,11 +106,44 @@ class StakeholderControllerTest {
     }
 
     @Test
+    void testUpdateWithInvalidId() throws Exception {
+        StakeholderUpdateRequest updateRequest = new StakeholderUpdateRequest();
+
+        Mockito.when(service.getById(-1L)).thenThrow(new IllegalArgumentException());
+
+        mockMvc.perform(put(URL + "/" + -1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updateRequest)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void testUpdateWithStakeholderNotFound() throws Exception {
+        StakeholderUpdateRequest updateRequest = new StakeholderUpdateRequest();
+
+        Mockito.when(service.getById(1L)).thenThrow(new NullPointerException());
+
+        mockMvc.perform(put(URL + "/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updateRequest)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
     void testDelete() throws Exception {
         Mockito.doNothing().when(service).remove(stakeholder);
         mockMvc.perform(delete(URL + "/" + stakeholder.getId()))
                 .andExpect(status().isOk());
     }
+
+    @Test
+    void testDeleteWithInvalidId() throws Exception {
+        Mockito.doThrow(new NullPointerException()).when(service).remove(null);
+
+        mockMvc.perform(delete(URL + "/" + -1L))
+                .andExpect(status().isBadRequest());
+    }
+
 
     @Test
     void testFindById() throws Exception {
@@ -90,9 +153,29 @@ class StakeholderControllerTest {
     }
 
     @Test
+    void testFindByIdWithException() throws Exception {
+        Mockito.when(service.getById(-1L)).thenThrow(NullPointerException.class);
+        mockMvc.perform(get(URL + "/" + -1L))
+                .andExpect(status().isBadRequest());
+    }
+
+
+    @Test
     void testList() throws Exception {
         Mockito.when(service.list(stakeholder.getType())).thenReturn(Collections.singletonList(stakeholder));
         mockMvc.perform(get(URL + "?type=" + stakeholder.getType())).andExpect(status().isOk());
+    }
+
+    @Test
+    void testListByNull() throws Exception {
+        Mockito.when(service.list(null)).thenReturn(Collections.singletonList(stakeholder));
+        mockMvc.perform(get(URL))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testListByInvalidType() throws Exception {
+        mockMvc.perform(get(URL + "?type=akladhsf")).andExpect(status().isBadRequest());
     }
 
 }

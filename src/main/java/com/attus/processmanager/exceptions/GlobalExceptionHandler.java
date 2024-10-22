35983daa7dc62e744393aad76672fee43dcacf00
kauan-cancelier <kauanmellocancelier2005@gmail.com>
2024-10-22 12:55:14 -0300

    Criado capturador global de exceções de violação de integridade referência de dados.

---
+++ src/main/java/com/attus/processmanager/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.attus.processmanager.exceptions;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleForeignKeyViolation(DataIntegrityViolationException ex) {
+        log.error("DataIntegrityVilationException caught: {}",ex.getMessage());
+        return new ResponseEntity<>("Unable to delete as there are records related to this entity. ", HttpStatus.BAD_REQUEST);
+    }
+
+}
